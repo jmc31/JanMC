d5b93f713035015a492c4f8f8c5849c1f3768b56
AdorableFetus <145571999+jmc31@users.noreply.github.com>
2024-02-01 19:32:26 +0800

    Add files via upload

---
+++ 4 arithmetic applications.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Math
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int Sub(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public int Multi(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public int Div(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Math math = new Math();
+        int num1, num2;
+
+        Console.WriteLine("Please enter 1st number = ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter 2nd number = ");
+        num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Sum is = " + math.Add(num1, num2));
+        Console.WriteLine("Difference is = " + math.Sub(num1, num2));
+        Console.WriteLine("Product is = " + math.Multi(num1, num2));
+        Console.WriteLine("Quotient is = " + math.Div(num1, num2));
+    }
+}
